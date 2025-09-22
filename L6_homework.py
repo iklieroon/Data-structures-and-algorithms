d03e8a4cba8093f8c6394dcc99271f51c1de66e3
iklieroon <rohanpras20@gmail.com>
2025-09-22 18:53:23 +0100

    Add files via upload

---
+++ L6_homework.py
@@ -0,0 +1,34 @@
+def merge(arr,lo,mid,hi):
+    new=[]
+    start1=lo
+    start2=mid+1
+    while start1<=mid and start2<=hi:
+        if arr[start1]>arr[start2]:
+            new.append(arr[start1])
+            start1=start1+1
+        else:
+            new.append(arr[start2])
+            start2=start2+1
+    while start1<=mid:
+        new.append(arr[start1])
+        start1+=1
+    while start2<=hi:
+        new.append(arr[start2])
+        start2+=1
+    k=0
+    for i in range(lo,hi+1):
+        arr[i]=new[k]
+        k+=1
+
+def msort(arr,lo,hi):
+    if lo<hi:
+        mid=(lo+hi)//2
+        msort(arr,lo,mid)
+        msort(arr,mid+1,hi)
+        merge(arr,lo,mid,hi)
+
+arr=[1,2,3,4,5]
+n=len(arr)
+msort(arr,0,n-1)
+
+print(arr)
