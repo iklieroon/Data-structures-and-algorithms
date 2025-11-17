91eeb6d25e636c922040df347df41f28a02f0c53
iklieroon <rohanpras20@gmail.com>
2025-11-17 18:44:20 +0000

    Update L10_Trees2.py

--- L10_Trees2.py
+++ L10_Trees2.py
@@ -19,3 +19,28 @@ def insert(root,k):
     else:
         root.rightnode=insert(root.rightnode,k)
     return root
+
+def search(root,key):
+    if key==root.data:
+        return root
+    elif root.data>key and root.leftnode is not None:
+        return search(root.leftchild,key)
+    elif root.data<key and root.rightnode is not None:
+        return search(root.rightchild,key)
+    else:
+        return -1
+
+v=int(input("how many elements do you want in tree"))
+root=None
+for i in range(v):
+    x=int(input("what element do you want in the node"))
+    root=insert(root,x)
+
+inordertraversal(root)
+
+key=int(input("enter key that is to be searched"))
+keynode=search(root,key)
+if keynode==-1:
+    print("key does not exist in the tree")
+else:
+    print("key exist",keynode.data)
